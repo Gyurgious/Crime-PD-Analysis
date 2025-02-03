026334d826b2402231658217e0ec85eceb09925d
Quang Le <106447805+Gyurgious@users.noreply.github.com>
2025-02-03 17:07:58 -0600

    data
    - crime count for each ZIPCODE

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ violent_crimes = ["Aggravated Assault", "Simple assault", "Forcible rape","robbe
 file_1 = "NIBRSPublicView2024.xlsx"
 print("test")
 
-df = pd.read_excel(file_1, nrows=300,usecols=["NIBRSDescription", "ZIPCode", "RMSOccurrenceHour"]) # only processes the first n rows of excel file
+df = pd.read_excel(file_1, nrows=10000,usecols=["NIBRSDescription", "ZIPCode", "RMSOccurrenceHour"] , dtype={"ZIPCode": str}) # only processes the first n rows of excel file
 
-print(df)
+# amount of crime at each ZIPCODE
+count_crime = df.groupby("ZIPCode")["NIBRSDescription"].count().reset_index()
+# Rename the column for clarity
+count_crime.columns = ["ZIPCode", "CrimeCount"]
+count_crime = count_crime.sort_values(by="CrimeCount", ascending=False)
+
+print(count_crime)
 print("yes")
+
+# categorize crime reports into zip codes
