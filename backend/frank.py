4d5d12ffb0ee6c7766dbf296a32fa460c05f6a03
Quang Le <106447805+Gyurgious@users.noreply.github.com>
2025-02-05 23:17:35 -0600

    React setup
    - added react into project

---
+++ backend/frank.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Load Excel file
+df = pd.read_excel("NIBRSPublicView2024.xlsx", nrows=200000, usecols=["StreetName", "NIBRSDescription", "Premise", "ZIPCode", "MapLongitude"], dtype={"ZIPCode": str})
+
+
+# Define the street and crime type you want to check
+specific_street = "FUQUA"   # Change to the street name you're looking for
+specific_crime = "Theft from motor vehicle"   # Change to the crime type you want to check
+premise = "Grocery, Supermarket"
+
+# Filter the dataset
+filtered_data = df[(df["StreetName"] == specific_street) & (df["Premise"] == premise)]
+
+crime_count = filtered_data.shape[0]
+# Display the filtered results
+print(filtered_data)
+print(crime_count)
